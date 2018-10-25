cf5a5a69094ea21843765aff0416f174a7594a68
Bogdan Crisan <bogdancrisan2303@gmail.com>
2018-10-25 18:42:26 +0300

    Add awaitable examples and modified coroutines 1 and 3 (also refactored coroutines to coroutines1)

---
+++ awaitables/awaitable3.py
@@ -0,0 +1,66 @@
+import asyncio
+import concurrent.futures
+
+"""
+Futures
+
+A Future is a special low-level awaitable object that represents an eventual result of an asynchronous operation.
+
+When a Future object is awaited it means that the coroutine will wait until the Future is resolved in some other place.
+
+Future objects in asyncio are needed to allow callback-based code to be used with async/await.
+
+Normally there is no need to create Future objects at the application level code.
+
+Future objects, sometimes exposed by libraries and some asyncio APIs, can be awaited:
+"""
+"""
+async def main():
+    await function_that_returns_a_future_object()
+
+    # this is also valid:
+    await asyncio.gather(
+        function_that_returns_a_future_object(),
+        some_python_coroutine()
+    )
+
+"""
+
+
+def blocking_io():
+    # File operations (such as logging) can block the
+    # event loop: run them in a thread pool.
+    with open('/dev/urandom', 'rb') as f:
+        return f.read(100)
+
+
+def cpu_bound():
+    # CPU-bound operations will block the event loop:
+    # in general it is preferable to run them in a
+    # process pool.
+    return sum(i * i for i in range(10 ** 7))
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    # Options:
+
+    # 1. Run in the default loop's executor:
+    result = await loop.run_in_executor(
+        None, blocking_io)
+    print('default thread pool', result)
+
+    # 2. Run in a custom thread pool:
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(
+            pool, blocking_io)
+        print('custom thread pool', result)
+
+    # 3. Run in a custom process pool:
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(
+            pool, cpu_bound)
+        print('custom process pool', result)
+
+asyncio.run(main())
--- coroutines/coroutines3.py
+++ coroutines/coroutines3.py
@@ -2,23 +2,47 @@ import asyncio
 import time
 
 
+# async def say_after(delay, what):
+#     await asyncio.sleep(delay)
+#     print(what)
+#     # needs debug, no info showed
+#     # async def main():
+#     task1 = asyncio.create_task(
+#         say_after(1, 'hello'))
+#
+#     task2 = asyncio.create_task(
+#         say_after(2, 'world'))
+#
+#     print('started at', time.strftime('%X'))
+#
+#     # Wait until both tasks are completed (should take
+#     # around 2 seconds.)
+#     await task1
+#     await task2
+#
+#     print('finished at', time.strftime('%X'))
+
 async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
-# needs debug, no info showed
-# async def main():
+
+print("test")
+
+
+async def main():
+    print("test")
     task1 = asyncio.create_task(
         say_after(1, 'hello'))
 
     task2 = asyncio.create_task(
         say_after(2, 'world'))
 
-    print('started at', time.strftime('%X'))
+    print(f"started at {time.strftime('%X')}")
 
     # Wait until both tasks are completed (should take
     # around 2 seconds.)
     await task1
     await task2
 
-    print('finished at', time.strftime('%X'))
+    print(f"finished at {time.strftime('%X')}")
